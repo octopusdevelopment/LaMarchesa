800a60f26b630915fce2504d372292d65b08391d
miyou995 <inter.taki@gmail.com>
2021-01-19 17:28:24 +0100

    pdf generation

--- octoshop/orders/admin.py
+++ octoshop/orders/admin.py
@@ -1,7 +1,41 @@
 from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from .models import Order, OrderItem
+import csv
+import datetime
+
+def export_to_csv(modelAdmin, request, queryset):
+    opts = modelAdmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Exporter en CSV'
 
 # Register your models here.
-from .models import Order, OrderItem
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">Detail</a>')
+
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
+
 
 class OrderItemInline(admin.TabularInline):
     model           = OrderItem
@@ -9,6 +43,7 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name','last_name' ,'phone' ,'email' ,'created' ,'updated' ,'paid']
+    list_display = ['id', 'first_name','last_name' ,'phone' ,'email' ,'created' ,'updated' ,'paid', order_detail, order_pdf]
     list_filter = ['paid','created' ,'updated']
     inlines = [OrderItemInline] 
+    actions = [export_to_csv]
--- octoshop/orders/models.py
+++ octoshop/orders/models.py
@@ -24,7 +24,7 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order    = models.ForeignKey(Order, verbose_name=("Commande"), on_delete=models.CASCADE)
+    order    = models.ForeignKey(Order,related_name='items', verbose_name=("Commande"), on_delete=models.CASCADE)
     product  = models.ForeignKey(Product, verbose_name=("Commande"), on_delete=models.CASCADE)
     price    = models.DecimalField( max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default = 1 )
