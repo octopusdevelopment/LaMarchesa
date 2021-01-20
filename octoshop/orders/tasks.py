923710f64e25cc00a0ca9b36c30d1e7a721db6f9
miyou995 <48582036+miyou995@users.noreply.github.com>
2021-01-20 06:30:49 +0100

    cairo problem laptop

--- octoshop/orders/tasks.py
+++ octoshop/orders/tasks.py
@@ -1,14 +1,33 @@
 from celery import task
+from io import BytesIO
 from django.core.mail import send_mail
-from .models import Order
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
+from django.conf import settings
+from .models import Order
+import weasyprint
 @task
 def order_created(order_id):
     order = Order.objects.get(id=order_id)
-    subjet = f'Commande N°: {order.id}'
+    subject = f'Commande N°: {order.id}'
     message = f'Chére {order.first_name},\n\n' \
               f'vous avez passer une commande avec succés' \
               f'votre identifiant de commande est le: {order.id}'
 
     mail_sent = send_mail(subject, message, 'inter.taki@gmail.com',[order.email])
     return mail_sent
+
+@task
+def order_validated(order_id):
+    order = Order.objects.get(id=order_id)
+    subject = f'OCTOSHOP - Commande ID° {order.id}'
+    message = 'Merci de nous avoir fais confiance.\n vueillez trouver votre facture en piece jointe'
+    email = EmailMessage(subject, message,'admin@octoshop.com', [order.email])
+
+    html = render_to_string('pdf.html', {"order":order})
+    out = BytesIO()
+    # stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+    weasyprint.HTML(string=html).write_pdf(out)
+    email.attach(f'order_{order.id}.pdf', out.getvalue(), 'application/pdf')
+    email.send()
