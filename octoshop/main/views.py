203c5e7a93535d4de7f6bbaa0776383ecf68c006
miyou995 <inter.taki@gmail.com>
2021-02-24 16:06:04 +0100

    hello 2

--- octoshop/main/views.py
+++ octoshop/main/views.py
@@ -4,7 +4,6 @@ from django.views.generic import TemplateView, ListView
 from cart.forms import CartAddProductForm
 
 
-
 class IndexView(TemplateView):
     template_name = "index.html"
     def get_context_data(self, **kwargs):
